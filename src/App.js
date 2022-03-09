c8efae59eaa1336f6b025e6bf4a554f16e7e5036
DavidSinz <davidsinz@gmx.net>
2022-03-09 23:32:56 +0100

    fetch api data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
-function App() {
+import { useEffect, useState } from "react";
+
+const App = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const url = "https://jsonplaceholder.typicode.com/users";
+
+    const fetchData = async () => {
+      try {
+        const res = await fetch(url);
+        const json = await res.json();
+        console.log(json);
+        setData(json);
+      } catch (error) {
+        console.log("error");
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div>Hello</div>
+    <div>
+      {data.map((user) => {
+        return <div key={user.id}>{user.name + user.email + user.address.city + user.address.street + user.address.zipcode}</div>;
+      })}
+    </div>
   );
-}
+};
 
 export default App;
